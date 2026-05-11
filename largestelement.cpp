2ae92e57cef04f50a5516ec56c1e6012a787182c
AnupCh37 <anup37@icloud.com>
2026-05-11 18:15:57 +0545

    first commit

---
+++ largestelement.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<iterator>
+
+int main()
+{
+   int arr[]= {3,6,5,7,67,2,1};
+  int length = std::size(arr);
+   int largest=0;
+   for (int i=0; i<length;i++){
+      if(arr[i]>largest){
+      largest =arr[i];
+   } }
+
+   std:: cout<<"The largest element \n"<<largest <<std::endl;
+    return 0 ;
+
+}
